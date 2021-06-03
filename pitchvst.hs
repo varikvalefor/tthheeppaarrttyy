b10a833c32ddbc7c807423eb8af0d39629d42137
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-06-02 20:31:36 -0400

    Update pitchvst.hs

--- pitchvst.hs
+++ pitchvst.hs
@@ -1,3 +1,7 @@
+-- This program is intentionally poorly documented; fixing the
+-- documentation and actually creating the program are left as exercises
+-- for the reader.
+
 import qualified Data.List as DL;
 import qualified Data.Function as DF;
 import qualified Numeric.FFT as FT;
@@ -21,45 +25,49 @@ data WaveType = Sin | Cos;
 
 ------------------------------------------------------------------------
 
+-- | createNewWinda creates a new X11 window and returns the tag of this
+-- X11 window.
 createNewWinda :: IO X11Winda;
 createNewWinda = return ExerciseForTheReader;
-{- createNewWinda creates a new X11 window and returns the tag of this
- - X11 window. -}
 
 fft :: [Sample] -> FFTOutput;
 fft = FT.fft . map cis;
 
+-- | For all FFT outputs k, fftToPitch equals the loudest pitch of k.
+-- Implementing fftToPitch is left as an exercise for the reader.
+-- Shut up and hack!
 fftToPitch :: FFTOutput -> Pitch;
 fftToPitch k = 0;
-{- For all FFT outputs k, fftToPitch equals the loudest pitch of k. -}
-{- Implementing fftToPitch is left as an exercise for the reader.
- - Shut up and hack! -}
 
+-- | readMicData is a stream of microphone samples.
+-- Implementing readMicData is left as an exercise for the reader. 
+-- The author recommends using the SDL library or an operating system-
+-- specific approach.
 readMicData :: IO [Sample];
 readMicData = return $ map (\a -> a * (sin a)) [1.0,1.1..];
-{- readMicData is a stream of microphone samples. -}
-{- Implementing readMicData is left as an exercise for the reader. 
- - The author recommends using the SDL library or an operating system-
- - specific approach. -}
 
+-- | For all natural numbers n, readSamp n equals n microphone samples.
 readSamp :: Int -> IO [Sample];
 readSamp k = readMicData >>= return . take k;
-{- For all natural numbers n, readSamp n equals n microphone samples. -}
 
+
+-- | For all [Sample] n, seqToPitch n equals the output of the sequence-
+-- to-pitch function on the input n.
 seqToPitch :: [Sample] -> Pitch;
 seqToPitch = fftToPitch . fft;
-{- For all [Sample] n, seqToPitch n equals the output of the sequence-
- - to-pitch function on the input n. -}
 
+-- | genCoords yields the coordinates of the graph.
 genCoords :: [Pitch] -> Giraffe;
 genCoords x = zip [0..length x - 1] x;
-{- genCoords yields the coordinates of the graph. -}
 
+-- | For all X11Winda k, for all Giraffe g, genGraph k g draws
+-- g to k, returning the pitch/y values of g.
+-- Implementing genGraph is left as an exercise for the reader.
+-- The author recommends using Chart or creating a custom library.
 genGraph :: X11Winda -> Giraffe -> IO [Pitch];
 genGraph w k = putStrLn "TODO: IMPLEMENT GRAPHING." >> return (map snd k);
-{- Implementing genGraph is left as an exercise for the reader.
- - The author recommends using Chart or creating a custom library. -}
 
+-- | mane is the main program loop.
 mane :: X11Winda -> [Double] -> IO ();
 mane w k = readSamp windowSize >>= doTheGraph w >>= loop w
   where
@@ -67,5 +75,6 @@ mane w k = readSamp windowSize >>= doTheGraph w >>= loop w
   append a b = a ++ [b]
   loop a b = mane a (take 50 b);
 
+-- | main is the program's entry point.  main initialises mane.
 main :: IO ();
 main = createNewWinda >>= \winda -> mane winda [];
